39aedd93552c744ace463d401a331361ec6d66f4
john <john.p.morgan@gmail.com>
2016-10-16 14:06:11 -0700

    add right click menu (nonfunctional so far)

--- qbrowse.py
+++ qbrowse.py
@@ -2,6 +2,7 @@ import tkinter as tk
 from tkinter import *
 from tkinter.ttk import *
 from awsHelpers import QueueManager
+import clickMenu
 
 import CredDialog
 
@@ -11,9 +12,10 @@ class Application(tk.Frame):
         self.master.title("SQS Browser")
         self.qm = QueueManager()
 
-        #self.create_menu()
+        self.create_menus()
 
         self.pack(fill=BOTH, expand=1)
+        self.create_layout()
         self.create_widgets()
         self.pack_widgets()
 
@@ -25,20 +27,43 @@ class Application(tk.Frame):
     #    self.file_menu.add_command(label="Add Credentials", command=self.addCreds)
     #    self.file_menu.add_command(label="Exit", command=self.exit)
 
+    def create_layout(self):
+        self.treeFrame = Frame(self)
+        self.filterFrame = Frame(self.treeFrame)
+        self.sendFrame = Frame(self)
+        self.buttonFrame = Frame(self.sendFrame)
+
     def pack_widgets(self):
-        pass
+        self.filterLabel.pack(side='left', padx=(2,4))
+        self.filterInput.pack(side='left', fill=X)
+        self.filterFrame.pack(side='top', fill=X, pady=(0,4))
+
+        self.tree.pack(side="top", fill=BOTH, expand=1)
+        self.treeFrame.pack(side='left', fill=BOTH, expand=1, padx=4, pady=(2,4))
+
+        self.details.pack(side="top", fill=tk.BOTH, expand=1)
+        self.sendFrame.pack(side='left', fill=tk.BOTH, expand=1, padx=(0,4), pady=(2,4))
+
+        self.refreshB.pack(side='left')
+        self.sendB.pack(side="left")
+        self.buttonFrame.pack(side='top', pady=(4,0))
+
+    def create_menus(self):
+        self.qmenu = Menu(self.master, tearoff=0)
+        self.qmenu.add_command(label='View Messages', command=self.say_hi)
+        self.qmenu.add_command(label='Purge', command=self.say_hi)
+        self.qmenu.add_command(label='Refresh', command=self.say_hi)
+
+        self.msgmenu = Menu(self.master, tearoff=0)
+        self.msgmenu.add_command(label='Delete', command=self.say_hi)
 
 
     def create_widgets(self):
-        treeFrame = Frame(self)
-        filterFrame = Frame(treeFrame)
-        filterLabel = Label(filterFrame, text='QueueFilter')
-        filterLabel.pack(side='left', padx=(2,4))
-        self.filterInput = Text(filterFrame, height=1)
-        self.filterInput.pack(side='left', fill=X)
+        self.filterLabel = Label(self.filterFrame, text='QueueFilter')
+        self.filterInput = Text(self.filterFrame, height=1)
         self.filterInput.bind("<<Modified>>", self.FilterChanged)
-        filterFrame.pack(side='top', fill=X, pady=(0,4))
-        self.tree = Treeview(treeFrame)
+
+        self.tree = Treeview(self.treeFrame)
         self.tree.column("#0", width=600, minwidth=300)
         ysb = Scrollbar(self, orient='vertical', command=self.tree.yview)
         self.tree.configure(yscroll=ysb.set)
@@ -48,29 +73,16 @@ class Application(tk.Frame):
             self.qm.qmap[qnode] = q
             if int(q['msgCount']) > 0:
                 self.tree.insert(qnode, 'end', text='')
-       # node1 = self.tree.insert("", 'end', text="test1")
-       # nodea = self.tree.insert("", 'end', text="test2")
-       # node2 = self.tree.insert(node1, 'end', text="test1")
-        self.tree.pack(side="top", fill=BOTH, expand=1)
 
         self.tree.bind("<<TreeviewSelect>>", self.TreeItemClick)
         self.tree.bind("<<TreeviewOpen>>", self.TreeItemExpand)
 
-        treeFrame.pack(side='left', fill=BOTH, expand=1, padx=4, pady=(2,4))
-        
-        sendframe = tk.Frame(self)
-        sendframe.pack(side='left', fill=tk.BOTH, expand=1, padx=(0,4), pady=(0,4))
+        self.details = tk.Text(self.sendFrame, width=60)
 
-        self.details = tk.Text(sendframe, width=60)
-        self.details.pack(side="top", fill=tk.BOTH, expand=1)
+        self.sendB = tk.Button(self.buttonFrame, text="Send Msg", command=self.Send)
+        self.refreshB = tk.Button(self.buttonFrame, text='Refresh', command=self.Refresh)
 
-        buttonFrame = tk.Frame(sendframe)
-        buttonFrame.pack(side='top', pady=(4,0))
-        self.sendB = tk.Button(buttonFrame, text="Send Msg", command=self.Send)
-        self.sendB.pack(side="left")
-
-        self.refreshB = tk.Button(buttonFrame, text='Refresh', command=self.Refresh)
-        self.refreshB.pack(side='left')
+        self.tree.bind("<Button-3>", self.popup)
 
     def fillMessages(self, node):
         for msg in self.qm.getMessages(self.qm.qmap[node]):
@@ -120,6 +132,17 @@ class Application(tk.Frame):
 
     def say_hi(self):
         self.qm.write(self.qm.queues[0], "hi there, everyone!")
+
+    def popup(self, event):
+        item = self.tree.identify('item', event.x, event.y)
+        if item == '':
+            return
+        self.tree.selection_set(item)
+        if item in self.qm.qmap:
+            self.qmenu.tk_popup(event.x_root, event.y_root)
+        else:
+            self.msgmenu.tk_popup(event.x_root, event.y_root)
+
 
 root = tk.Tk()
 root.geometry("900x580")
